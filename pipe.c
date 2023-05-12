db598398c6a53c118fd2da942d28e922d9bec27a
MinGeun Kim <skykimmg0627@gmail.com>
2023-05-12 07:42:42 +0000

    add functions

--- pipe.c
+++ pipe.c
@@ -12,7 +12,122 @@
 
 #include "minishell.h"
 
-int	pipe_and_fork(void)
+int	set_fds_malloc(char ***cmds, int **input_fds_out, int **output_fds_out)
 {
-	return (0);
+	int		cmds_len;
+
+	cmds_len = 0;
+	while (cmds[cmds_len] != NULL)
+		cmds_len++;
+	*input_fds_out = (int *)malloc(sizeof(int) * cmds_len);
+	if (*input_fds_out == NULL)
+	{
+		write(2, "malloc error\n", 14);
+		return (0);
+	}
+	*output_fds_out =  (int *)malloc(sizeof(int) * cmds_len);
+	if (*output_fds_out == NULL)
+	{
+		free(*input_fds_out);
+		write(2, "malloc error\n", 14);
+		return (0);
+	}
+	return (1);
+}
+
+int	get_input_fd(char **cmd)
+{
+	int	cmd_index;
+	int	fd;
+
+	cmd_index = 0;
+	while (cmd[cmd_index] != NULL)
+	{
+		if (ft_strncmp(cmd[cmd_index], "<\0", 2) == 0)
+		{
+			fd = open(cmd[cmd_index + 1], O_RDONLY);
+			if (fd < 0)
+				perror("open");
+			return (fd);
+		}
+		cmd_index++;
+	}
+	return (-1);
+}
+
+int		get_output_fd(char **cmd)
+{
+	int	cmd_index;
+	int	fd;
+
+	cmd_index = 0;
+	while (cmd[cmd_index] != NULL)
+	{
+		if (ft_strncmp(cmd[cmd_index], ">\0", 2) == 0)
+		{
+			fd = open(cmd[cmd_index + 1], O_WRONLY);
+			if (fd < 0)
+				perror("open");
+			return (fd);
+		}
+		cmd_index++;
+	}
+	return (-1);
+}
+
+void	open_files_and_set_fds(char ***cmds, int *input_fds_out, \
+								int *output_fds_out)
+{
+	int		cmds_len;
+	int		cmds_index;
+	int		*input_fds;
+
+	cmds_len = 0;
+	while (cmds[cmds_len] != NULL)
+		cmds_len++;
+	cmds_index = 0;
+	while (cmds[cmds_index] != NULL)
+	{
+		input_fds_out[cmds_index] = get_input_fd(cmds[cmds_index]);
+		output_fds_out[cmds_index] = get_output_fd(cmds[cmds_index]);
+		cmds_index++;
+	}
+}
+
+void	exec_command(void)
+{
+	pid_t	cpid;
+
+	cpid = fork();
+	if (cpid > 0)
+		return ;
+	else if (cpid < 0)
+	{
+		perror("fork");
+		exit(1);
+	}
+	// if (dup2(last_fd, STDIN_FILENO) < 0)
+	// 	perror("dup2");
+	// if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+	// 	perror("dup2");
+	//close(last_fd);
+	//close(pipefd[0]);
+	//close(pipefd[1]);
+	//exeve();
+	perror("execve");
+}
+
+int	operator_process(char ***cmds, char **envp)
+{
+	int	*input_fds;
+	int	*output_fds;
+
+	set_fds_malloc(cmds, &input_fds, &output_fds);
+	//redirections & open files fds
+	//pipes
+	//exec
+	//close fds
+	//unlink files
+	free(input_fds);
+	free(output_fds);
 }
