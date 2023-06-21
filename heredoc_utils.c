3a45c72bae9c1dd387697a49f6611872122c61e7
leeesooha <suha630@naver.com>
2023-06-21 13:42:24 +0900

    exit fix

--- heredoc_utils.c
+++ heredoc_utils.c
@@ -6,7 +6,7 @@
 /*   By: soohlee <soohlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:54:46 by soohlee           #+#    #+#             */
-/*   Updated: 2023/06/20 19:55:10 by soohlee          ###   ########.fr       */
+/*   Updated: 2023/06/21 11:01:38 by soohlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,27 @@ int	heredoc_unlink(char ****tokens)
 				unlink(ft_strchr(tokens[process_idx][0][redir_idx], ' ') + 1);
 			}
 		}
+	}
+	return (0);
+}
+
+int	make_heredocfile(char **filename)
+{
+	char	*str_num;
+	int		i;
+
+	i = 0;
+	while (1)
+	{
+		str_num = ft_itoa(i);
+		*filename = mi_strjoin(".heredoc_", str_num);
+		free(str_num);
+		str_num = 0;
+		if (access(*filename, F_OK | R_OK | W_OK) != 0)
+			break ;
+		free(*filename);
+		*filename = 0;
+		i++;
 	}
 	return (0);
 }
